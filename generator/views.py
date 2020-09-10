d342f7f35fc477ba78cc90ee030c9e31514777d0
Joe Klemmer <joe.klemmer@gmail.com>
2020-09-10 12:36:57 -0400

    Adding the passwd options to the form

--- generator/views.py
+++ generator/views.py
@@ -5,8 +5,7 @@ from django.http import HttpResponse
 # Create your views here.
 
 def home(request):
-    return render(request, 'generator/home.html', 
-                  {'password': 'Wtlnc,r2te0tb.'})
+    return render(request, 'generator/home.html')
 
 def password(request):
     return render(request, 'generator/password.html')
